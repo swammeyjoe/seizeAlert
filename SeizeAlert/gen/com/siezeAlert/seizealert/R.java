ce8361889c0fb81eecc3e2b326e5853bec02386f
freddysalinas801 <freddysalinas801@gmail.com>
2013-11-16 07:49:25 -0600

    Trying to commit

--- SeizeAlert/gen/com/siezeAlert/seizealert/R.java
+++ SeizeAlert/gen/com/siezeAlert/seizealert/R.java
@@ -53,15 +53,16 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050005;
-        public static final int alright=0x7f05000d;
+        public static final int alright=0x7f05000e;
         public static final int app_name=0x7f050000;
-        public static final int emergency_contacts=0x7f05000c;
+        public static final int emergency_contacts=0x7f05000d;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
-        public static final int next=0x7f05000a;
-        public static final int seize_alert_description=0x7f050009;
-        public static final int seize_alert_title=0x7f050008;
-        public static final int title_activity_seizure_confirmation=0x7f05000b;
+        public static final int next=0x7f05000b;
+        public static final int notification_pressed=0x7f050008;
+        public static final int seize_alert_description=0x7f05000a;
+        public static final int seize_alert_title=0x7f050009;
+        public static final int title_activity_seizure_confirmation=0x7f05000c;
         public static final int title_activity_setup_get_contacts=0x7f050007;
         public static final int title_activity_setup_get_name=0x7f050006;
         public static final int title_activity_setup_intro=0x7f050004;
