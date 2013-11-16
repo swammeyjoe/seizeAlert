6542c88845b624e4f3fe222b712b6c82edbc64f2
unknown <zoruiz24@gmail.com>
2013-11-15 20:58:21 -0600

    Added simple texting activity that will use the shared preferences data to send a text message to one of the saved numbers. We will need to create these preferences on launch of the app the first time.

---
+++ SeizeAlert/src/com/siezeAlert/seizealert/TextingActivity.java
@@ -0,0 +1,25 @@
+package com.siezeAlert.seizealert;
+
+import android.telephony.*;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.view.Menu;
+
+@SuppressLint("UnlocalizedSms")
+public class TextingActivity extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		String name, number;
+		SmsManager texting = SmsManager.getDefault();
+		SharedPreferences data = getPreferences(MODE_PRIVATE);
+		name = data.getString("Name", "");
+		if (data.contains("Number1")){
+			number = data.getString("Number1", "");			
+		} else { 
+			number = "1111111111";
+		}
+		
+		texting.sendTextMessage(number, null, "HELP", null, null);
+	}
+}
